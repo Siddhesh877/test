5a099e40c8ade07e4ff88737ea7021c0c726eaa0
siddhesh <siddheshparate877@gmail.com>
2022-11-13 09:14:29 +0000

    Create tasks.json and hello.js

---
+++ hello.js
@@ -0,0 +1,76 @@
+// console.log('hello world');
+// const a=10;
+// const b=11;
+// if(a>5)
+// {
+//     console.log(a+" is greater than 5");
+// }
+// else if(b>5)
+// {
+//     console.log(b+" is greater than 5");
+// }
+// else
+// {
+//     console.log(a+" 5 "+b+" are smaller than 5");
+// }
+
+// const a=10;
+// for (let i=0;i<a;i++)
+// {
+//     console.log(i);
+// }
+
+const a = {
+  b: undefined,
+  c: null,
+  d: {
+    b: 1,
+    c: 2,
+  },
+};
+//console.log(a);
+
+// try{
+//     console.log(a.d.c);
+// }catch(err){
+//     console.log(err);
+//     console.log("failed accessing a.b.c");
+// }
+// function square(x){
+//     return x*x;
+// }
+
+// console.log(square(9));
+var cowsay = require("cowsay");
+var joke = require("one-liner-joke");
+var RandomJoke = joke.getRandomJoke();
+
+console.log(
+  cowsay.say({
+    text: RandomJoke.body,
+    e: "oO",
+    T: "U",
+  })
+);
+square(9);
+var cowsay = require("cowsay");
+var joke = require("one-liner-joke");
+var RandomJoke = joke.getRandomJoke();
+
+console.log(
+  cowsay.say({
+    text: RandomJoke.body,
+    e: "oO",
+    T: "U",
+  })
+);
+(square(9));
+var cowsay = require("cowsay");
+var joke=require('one-liner-joke');
+var RandomJoke=joke.getRandomJoke();
+
+console.log(cowsay.say({
+    text : RandomJoke.body,
+    e : "oO",
+    T : "U"
+}));
